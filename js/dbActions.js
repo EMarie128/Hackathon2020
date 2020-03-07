5d77c633ee0806479d37af61f7a97875ea6f11f7
Mat Davidson <matdavidson88@gmail.com>
2020-03-07 13:52:20 -0500

    Got Insert user working properly

--- js/dbActions.js
+++ js/dbActions.js
@@ -3,7 +3,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "35.237.246.137",
   user: "root",
-  password: ""
+  password: "",
+  database: "hackathon"
 });
 
 // con.connect(function(err) {
@@ -15,8 +16,8 @@ function addUser(user){
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-
-    var sql = "INSERT INTO users (UserID, FirsteName, MiddleName, LastName, Email, BirthDate, UserPassword, Height, Weight) VALUES ?";
+    
+    var sql = "INSERT INTO users (UserID, FirstName, MiddleName, LastName, Email, BirthDate, UserPassword, Height, Weight) VALUES (?)";
     con.query(sql, [user], function (err, result) {
       if (err) throw err;
       console.log("1 record inserted: ", sql);
@@ -24,6 +25,6 @@ function addUser(user){
   });
 }
 
-let per = ['9999','Mat', 'f', 'Davidson', 'bloosh', '1988-09-08', 'poopsicle','76','245'];
-addUser(per);
-//export {addUser};
+// let per = [9999,"Mat", 'f', "Davidson", "bloosh", '1988-09-08', 'poopsicle',76,245];
+// addUser(per);
+export {addUser};
