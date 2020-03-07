d1be79096e9324ea913f29d76b294194c12dae6e
WallaceColeman <wallardcoleman@gmail.com>
2020-03-07 14:09:06 -0500

    added populate activity

--- js/populate/populate.js
+++ js/populate/populate.js
@@ -1,6 +1,7 @@
+let entries = [];
 function populate(num){
-    let entries = [];
-    let fNamels = ["Wallace","Emily","Mat","Arron","Alice","Bobby","Beth","Cate","Charly","Dalton","Dina","Erin","Eva","Franklin","Felicia","Gary","Gwen","Henry","Hellen","Isabel","Ivan","Jason","Jen","Karl","Katie","Luis","Lauren","Marty","Monica","Owen","Oliva","Patric","Patricia","Quin","Robert","Rose","Steven","Sara","Terry","Talya","Ustof","Uriel","Vince", "Violet", "Wilma"];
+    
+    let fNames = ["Wallace","Emily","Mat","Arron","Alice","Bobby","Beth","Cate","Charly","Dalton","Dina","Erin","Eva","Franklin","Felicia","Gary","Gwen","Henry","Hellen","Isabel","Ivan","Jason","Jen","Karl","Katie","Luis","Lauren","Marty","Monica","Owen","Oliva","Patric","Patricia","Quin","Robert","Rose","Steven","Sara","Terry","Talya","Ustof","Uriel","Vince", "Violet", "Wilma"];
     let lNames = ["Alester", "Boris", "Coleman", "Davidson", "Ericson", "Falmer", "Gamer", "Hall", "Igris", "Jacobson", "Karrison", "Law", "Martin", "Nobody", "Owens", "Perry", "Quiter", "Roberts", "Stencils", "Terance", "Ulfrik", "Vance", "Wallace","Xero"];
     for(let i = 0; i < num; i++){
         let year = Math.floor(Math.random() * 75 + 1940);
@@ -15,5 +16,19 @@ function populate(num){
         console.log(x);
         //addUser(x);
     }
-    console.log(entries);
+    popActivity();
+}
+
+function popActivity(){
+    for(let i = 0; i < entries.length; i++){
+        for(let j = 1 ; j <= 7 ; j++){
+            let id = entries[i][0];
+            let date = "2020-03-0" + j;
+            let steps = Math.floor(Math.random() * 2000) + 1000;
+            let calIn = Math.floor(Math.random() * 2000) + 1000;
+            let sleep = Math.floor(Math.random() * 6) + 4;
+            let x = [id, date, steps, calIn, sleep];
+            console.log(x);
+        }
+    }
 }
